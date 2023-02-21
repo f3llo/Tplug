579f57d4ce257c7b07ac86b5b292e9a9bbe9cdc9
FELLO1982 <106307101+FELLO1982@users.noreply.github.com>
2023-02-21 11:17:31 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,41 @@
-#credit tech with tim
 import socket
+import threading
+import tkinter as tk
+import keyboard
+
+root = tk.Tk()
+
+def show_entry_fields():
+    print("Username: %s\nPort: %s" % (e1.get(), e2.get()))
+    global NAME 
+    NAME = e1.get()
+    global PORT
+    PORT = e2.get()
+    PORT = int(PORT)
+
+tk.Label(root, text="Username").grid(row=0)
+tk.Label(root, text="Port").grid(row=1)
+
+e1 = tk.Entry(root)
+e2 = tk.Entry(root)
+
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+
+def destroy():
+    root.destroy
+
+b1 = tk.Button(root, text='submit', command=lambda:[destroy(), show_entry_fields()])
+b1.grid(row=3, column=1, sticky=tk.W,pady=4)
+b2 = tk.Button(root, text='Done', command=root.destroy)
+b2.grid(row=4, column=0)
+
+
+root.mainloop()
+
+
 
 HEADER = 64
-PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = socket.gethostbyname(socket.gethostname())
@@ -10,6 +43,7 @@ ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+
 
 def send(name, msg):
     message = msg.encode(FORMAT)
@@ -20,8 +54,31 @@ def send(name, msg):
     client.send(message)
     print(client.recv(2048).decode(FORMAT))
 
+
 connected = True
+window = tk.Tk()
+
+label = tk.Label(window, text=f"Connected to: {PORT}")
+label.grid(row=2, column=2)
+
+#b1 = tk.Button(window, text="DISCONNECT", command=send("", DISCONNECT_MESSAGE))
+#b1.grid(row=0,column=0)
+
+messages = []
+
+e1 = tk.Entry(window)
+e1.grid(row=1,column=0)
 while connected:
-    #send_input = send(input())
-    #send(f"[CLIENT{ADDR}]", send_input)
-    send("[CLIENT]", input())
+    if keyboard.is_pressed("q"):
+        message = e1.get()
+        messages.append(message)
+        send(NAME, message)
+        break
+
+#while connected:
+    #send(NAME, NAME)
+    #send(f"[CLIENT{NAME}]", input(f"[SELF: {NAME}]"))
+
+
+
+window.mainloop()
